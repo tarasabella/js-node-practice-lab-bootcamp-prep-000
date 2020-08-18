912282eea3682ac187ae3dfbcb71e0b23a9a1073
Tara Sabella <tarasabella@gmail.com>
2020-08-18 18:40:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 // write your code below!
 
-var name = "Janet"
-var height = 60
-
-console.log("Name:")
-console.log(name)
-
-console.log("Height:")
-console.log(height + 1)
+var name = "Joe"
+var height = 74
